7ee896ce71e0105b36a0a7cfa34cbc8933b3a830
loveky <ylzcylx@gmail.com>
2018-11-08 16:21:11 +0800

    fix(paths): fix wrong logic

--- packages/rocketact-dev-utils/src/paths.spec.ts
+++ packages/rocketact-dev-utils/src/paths.spec.ts
@@ -10,4 +10,32 @@ describe("getValidEntries", () => {
       )
     ).toEqual({});
   });
+
+  it("should return valid entries", () => {
+    const entries = getValidEntries(
+      path.resolve(process.cwd(), "./test/fixture/hasValidEntry")
+    );
+
+    expect(entries).toMatchObject({
+      a: {
+        js: expect.stringMatching(/a\.js$/),
+        html: expect.stringMatching(/a\.html$/)
+      },
+      b: {
+        js: expect.stringMatching(/b\.jsx$/),
+        html: expect.stringMatching(/b\.html$/)
+      },
+      c: {
+        js: expect.stringMatching(/c\.ts$/),
+        html: expect.stringMatching(/c\.html$/)
+      },
+      d: {
+        js: expect.stringMatching(/d\.tsx$/),
+        html: expect.stringMatching(/d\.html$/)
+      }
+    });
+
+    expect(path.isAbsolute(entries.a.js)).toBeTruthy();
+    expect(path.isAbsolute(entries.a.html)).toBeTruthy();
+  });
 });
--- packages/rocketact-dev-utils/src/paths.ts
+++ packages/rocketact-dev-utils/src/paths.ts
@@ -7,40 +7,56 @@ interface IEntries {
   [key: string]: string;
 }
 
+interface IValidEntries {
+  [key: string]: {
+    js: string;
+    html: string;
+  };
+}
+
 const appRoot = fs.realpathSync(process.cwd());
 
 const resolveToAppRoot = (p: string) => path.resolve(appRoot, p);
 
-const getJsEntries: (p: string) => IEntries = R.pipe(
-  R.flip(R.nAry(2, path.resolve))("./src/js/pages/"),
-  fs.readdirSync,
-  R.filter(R.test(/\.[tj]sx?$/)),
-  R.converge(R.zipObj, [
-    R.pipe(
-      path.basename,
-      R.replace(/\.[tj]sx?$/, "")
-    ),
-    R.identity
-  ])
-);
+const getJsEntries = (p: string): IEntries => {
+  return R.pipe(
+    R.flip(R.nAry(2, path.resolve))("./src/js/pages/"),
+    fs.readdirSync,
+    R.map(R.curry(R.nAry(3, path.resolve))(p, "./src/js/pages")),
+    R.filter(R.test(/\.[tj]sx?$/)),
+    R.converge(R.zipObj, [
+      R.map(
+        R.pipe(
+          path.basename,
+          R.replace(/\.[tj]sx?$/, "")
+        )
+      ),
+      R.identity
+    ])
+  )(p);
+};
 
-const getHtmlEntries: (p: string) => IEntries = R.pipe(
-  R.flip(R.nAry(2, path.resolve))("./src/html/"),
-  fs.readdirSync,
-  R.filter(R.test(/\.html$/)),
-  R.converge(R.zipObj, [
-    R.pipe(
-      path.basename,
-      R.replace(/\.html$/, "")
-    ),
-    R.identity
-  ])
-);
+const getHtmlEntries = (p: string): IEntries => {
+  return R.pipe(
+    R.flip(R.nAry(2, path.resolve))("./src/html/"),
+    fs.readdirSync,
+    R.map(R.curry(R.nAry(3, path.resolve))(p, "./src/html")),
+    R.filter(R.test(/\.html$/)),
+    R.converge(R.zipObj, [
+      R.map(
+        R.pipe(
+          path.basename,
+          R.replace(/\.html$/, "")
+        )
+      ),
+      R.identity
+    ])
+  )(p);
+};
 
-const getValidEntries: (p: string) => IEntries = p => {
+const getValidEntries: (p: string) => IValidEntries = p => {
   const jsEntries = getJsEntries(p);
   const htmlEntries = getHtmlEntries(p);
-
   return R.fromPairs(
     R.map(
       (entryName: string) => [
