0d791cf2604a9e6037c7ae612c932090dc6d0170
loveky <ylzcylx@gmail.com>
2018-11-11 16:38:14 +0800

    build: update jest config

--- packages/rocketact-dev-utils/jest.config.js
+++ packages/rocketact-dev-utils/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  collectCoverageFrom: ["src/**/*.{js,ts}"]
+  collectCoverageFrom: ["src/**/*.{js,ts}"],
+  testMatch: [  "**/test/?(*.)+(spec|test).+(j|t)s?(x)" ]
 };
