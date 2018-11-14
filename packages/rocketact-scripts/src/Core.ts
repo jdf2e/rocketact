a59b033a14d5af6abbd4acd91d2d3d284c4a7765
loveky <ylzcylx@gmail.com>
2018-11-14 19:55:14 +0800

    feat: WIP

--- packages/rocketact-scripts/src/Core.ts
+++ packages/rocketact-scripts/src/Core.ts
@@ -1,6 +1,7 @@
 import WebpackChain from "webpack-chain";
 import { error } from "rocketact-dev-utils";
 import minimist from "minimist";
+import glob from "glob";
 
 import CoreAPI from "./CoreAPI";
 
@@ -25,7 +26,16 @@ class Core {
     this.webpackChainFns.forEach(fn => fn(this.webpackChain));
   }
 
+  resolveBuiltInPlugins() {
+    const builtInPlugins = glob.sync("commands/*.*,./config/**/*.*", {
+      cwd: __dirname
+    });
+    console.log(builtInPlugins);
+  }
+
   run(command: string, args: minimist.ParsedArgs) {
+    this.resolveBuiltInPlugins();
+    this.applyWebpackChainFns();
     if (!this.commands["command"]) {
       console.log(error(`Subcommand [${command}] does not exist!`));
       process.exit(1);
