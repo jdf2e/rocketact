c505196ccfeec0b875720fcc59b2db10e1ec4381
loveky <ylzcylx@gmail.com>
2018-11-19 17:13:02 +0800

    feat: change path variable to path helper functions

--- packages/rocketact-dev-utils/src/paths.ts
+++ packages/rocketact-dev-utils/src/paths.ts
@@ -1,12 +1,12 @@
 import path from "path";
 import fs from "fs";
 
-const appRoot = fs.realpathSync(process.cwd());
+const appRoot = () => fs.realpathSync(process.cwd());
 
-const resolveToAppRoot = (p: string) => path.resolve(appRoot, p);
+const resolveToAppRoot = (p: string) => path.resolve(appRoot(), p);
 
-const appSrc = resolveToAppRoot("src");
-const appBuild = resolveToAppRoot("build");
-const appPackageJson = resolveToAppRoot("package.json");
+const appSrc = () => resolveToAppRoot("src");
+const appBuild = () => resolveToAppRoot("build");
+const appPackageJson = () => resolveToAppRoot("package.json");
 
-export { resolveToAppRoot, appSrc, appBuild, appPackageJson };
+export { resolveToAppRoot, appSrc, appBuild, appPackageJson, appRoot };
