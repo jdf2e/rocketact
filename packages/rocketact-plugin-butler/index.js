9b18aa59c33697bbede22ce60950a972ba17c75b
loveky <ylzcylx@gmail.com>
2018-12-24 21:11:24 +0800

    feat: first working version

---
+++ packages/rocketact-plugin-butler/index.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
+const createHtmlWebpackPluginInstance = require("rocketact-scripts/dist/utils/createHtmlWebpackPluginInstance")
+  .default;
+
+const { resolveToAppRoot } = require("rocketact-dev-utils");
+
+module.exports = api => {
+  const jsEntries = {};
+  fs.readdirSync(resolveToAppRoot("./src/js/pages"))
+    .filter(p => p.match(/\.[tj]sx?$/))
+    .map(p => path.join(resolveToAppRoot("./src/js/pages"), p))
+    .forEach(p => {
+      const entryName = path.basename(p).replace(/\.[tj]sx?$/, "");
+      jsEntries[entryName] = p;
+    });
+
+  const htmlEntries = {};
+  fs.readdirSync(resolveToAppRoot("./src/html"))
+    .filter(p => p.match(/\.html$/))
+    .map(p => path.join(resolveToAppRoot("./src/html"), p))
+    .forEach(p => {
+      const entryName = path.basename(p).replace(/\.html$/, "");
+      htmlEntries[entryName] = p;
+    });
+
+  api.chainWebpack(webpackChain => {
+    // clear default entries and HtmlWebpackPlugin instances
+    webpackChain.entryPoints.clear();
+
+    Object.keys(webpackChain.plugins.entries()).forEach(pluginName => {
+      if (pluginName.match(/^HtmlWebpackPlugin\-/)) {
+        webpackChain.plugins.delete(pluginName);
+      }
+    });
+
+    // add entries and HtmlWebpackPlugin instances
+    Object.keys(jsEntries).forEach(entryName => {
+      if (htmlEntries[entryName]) {
+        webpackChain.entry(entryName).add(jsEntries[entryName]);
+
+        webpackChain.plugin(`HtmlWebpackPlugin-${entryName}`).use(
+          createHtmlWebpackPluginInstance({
+            entryName,
+            template: htmlEntries[entryName]
+          })
+        );
+      }
+    });
+  });
+};
