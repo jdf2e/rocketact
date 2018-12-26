cc3b629e3c566b7ec064389d0861ff66668f1cbb
beanlee <irobot039@gmail.com>
2018-12-26 18:11:51 +0800

    feat(packageUtil): fix review code and promise return

--- packages/rocketact-dev-utils/src/packageUtil.ts
+++ packages/rocketact-dev-utils/src/packageUtil.ts
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const path = require("path");
-const execa = require("execa");
+import * as fs from "fs";
+import * as path from "path";
+import * as execa from "execa";
 
 interface IPackageUtil {
   /**
@@ -24,11 +24,18 @@ interface IPackageUtil {
   uninstall: (packageName: string) => Promise<IExecaReturn>;
 }
 
-interface IExecaReturn {
+/**
+ * process 执行返回值
+ */
+export interface IExecaReturn {
+  cmd: string;
   code: number;
-  signal: string;
-  stdout: string;
+  failed: boolean;
+  killed: boolean;
+  signal: string | null;
   stderr: string;
+  stdout: string;
+  timedOut: boolean;
 }
 
 /**
@@ -41,7 +48,34 @@ interface IExecaReturn {
  * TODO: 安装过程 loading 和 emoji
  */
 class PackageUtil implements IPackageUtil {
-  constructor() {}
+  constructor() {
+    // TODO: singleton
+  }
+
+  /**
+   * 安装依赖
+   * @param {string} packageName 依赖包名称
+   * @param {string} versionOrTag 依赖包版本号或tag
+   * @param {boolean} isDev 是否为开发依赖，默认 ture
+   * @returns promise
+   */
+  public install(
+    packageName?: string,
+    version?: string,
+    isDev = true
+  ): Promise<IExecaReturn> {
+    return this.processInstall(packageName, version, isDev);
+  }
+
+  /**
+   * 删除依赖
+   *
+   * @param {string} packageName 依赖包名称
+   * @returns Promise
+   */
+  public uninstall(packageName: string): Promise<IExecaReturn> {
+    return this.processUninstall(packageName);
+  }
 
   private async hasYarn(): Promise<boolean> {
     try {
@@ -58,9 +92,9 @@ class PackageUtil implements IPackageUtil {
         path.resolve(`${process.cwd()}`, "yarn.lock"),
         fs.constants.R_OK
       );
-      return true;
+      return Promise.resolve(true);
     } catch (err) {
-      return false;
+      return Promise.resolve(false);
     }
   }
 
@@ -69,21 +103,26 @@ class PackageUtil implements IPackageUtil {
     versionOrTag?: string,
     isDev = true
   ): Promise<IExecaReturn> {
-    let args: any = [];
+    const args: any = [];
 
     let canUseYarn = await this.hasYarn();
     const hasYarnLock = await this.hasYarnLock();
 
-    let command = canUseYarn ? "yarn" : "npm";
+    const command = canUseYarn ? "yarn" : "npm";
     if (!packageName) {
-      return await execa.shellSync(`${command} install`);
+      try {
+        const result = await execa.shell(`${command} install`);
+        return Promise.resolve({ ...(result as IExecaReturn) });
+      } catch (error) {
+        return Promise.reject({ error });
+      }
     }
 
-    canUseYarn = canUseYarn && hasYarnLock; // 本工程内以前未使用yarn管理
+    canUseYarn = canUseYarn && hasYarnLock; // 本工程内以前未使用 yarn 管理
 
     if (canUseYarn) {
       args.push("add");
-      args.push(`${packageName}${versionOrTag ? `@${versionOrTag}` : " -E"}`); // FIXME: ？确保项目安装依赖稳定使用固定版本
+      args.push(`${packageName}${versionOrTag ? `@${versionOrTag}` : ""}`);
 
       args.push(isDev ? "-D" : "");
 
@@ -91,19 +130,18 @@ class PackageUtil implements IPackageUtil {
       args.push(process.cwd());
     } else {
       args.push("install");
-      args.push(
-        `${packageName}${versionOrTag ? `@${versionOrTag}` : " -E"}` // 精确版本
-      );
+      args.push(`${packageName}${versionOrTag ? `@${versionOrTag}` : ""}`);
       args.push(isDev ? "-D" : "-P");
     }
 
     let runReturn: IExecaReturn;
     try {
-      runReturn = await execa.shellSync(`${command} ${args.join(" ")}`);
+      runReturn = execa.shellSync(`${command} ${args.join(" ")}`);
+      return Promise.resolve(runReturn);
     } catch (error) {
       runReturn = { ...error };
+      return Promise.reject(runReturn);
     }
-    return runReturn;
   }
 
   private async processUninstall(packageName: string): Promise<IExecaReturn> {
@@ -111,7 +149,7 @@ class PackageUtil implements IPackageUtil {
       console.log(
         "  * Please specify the package name which you want to remove!"
       );
-      process.exit(1);
+      return Promise.reject({});
     }
 
     const canUseYarn = await this.hasYarn();
@@ -120,25 +158,13 @@ class PackageUtil implements IPackageUtil {
 
     let runReturn: IExecaReturn;
     try {
-      runReturn = await execa.shellSync(`${command} ${packageName}`);
+      runReturn = await execa.shell(`${command} ${packageName}`);
+      return Promise.resolve(runReturn);
     } catch (error) {
       // yarn remove 删除一个不存在的包会报错，npm unisntall 并不会
       runReturn = { ...error };
+      return Promise.reject(runReturn);
     }
-
-    return runReturn;
-  }
-
-  public install(
-    packageName?: string,
-    version?: string,
-    isDev = true
-  ): Promise<IExecaReturn> {
-    return this.processInstall(packageName, version, isDev);
-  }
-
-  public uninstall(packageName: string): Promise<IExecaReturn> {
-    return this.processUninstall(packageName);
   }
 }
 
