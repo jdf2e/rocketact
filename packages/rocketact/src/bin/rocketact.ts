28f4d7d8bc75e0a9ce73ac2bc7552d813b5441b6
beanlee <irobot039@gmail.com>
2019-01-04 15:23:22 +0800

    style: use default prettier config for this project, remove .prettierrc, and format code

--- packages/rocketact/src/bin/rocketact.ts
+++ packages/rocketact/src/bin/rocketact.ts
@@ -9,29 +9,31 @@ import {
   normalBlock,
   successBlock,
   packageUtil
-} from 'rocketact-dev-utils';
+} from "rocketact-dev-utils";
 
-import * as fs from 'fs-extra';
-import * as path from 'path';
-import * as os from 'os';
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as os from "os";
 
-const validatePackageName = require('validate-npm-package-name'); // tslint:disable-line
+const validatePackageName = require("validate-npm-package-name"); // tslint:disable-line
 
-import program from 'commander';
+import program from "commander";
 
 /** 项目名称 */
 let projectName: string | undefined;
 
-const projectMainPkg = fs.readJsonSync(path.resolve(__dirname, '../../package.json'));
+const projectMainPkg = fs.readJsonSync(
+  path.resolve(__dirname, "../../package.json")
+);
 
 program
-  .version(projectMainPkg.version, '-v, --version, -V')
-  .arguments('<project-directory>')
-  .description('rocketact will help you create <project-directory> [options]')
-  .option('create', 'Create an awesome app')
+  .version(projectMainPkg.version, "-v, --version, -V")
+  .arguments("<project-directory>")
+  .description("rocketact will help you create <project-directory> [options]")
+  .option("create", "Create an awesome app")
   .allowUnknownOption()
-  .on('--help', () => {
-    console.log(`Only ${infoBlock('<project-directory>')} is required.`);
+  .on("--help", () => {
+    console.log(`Only ${infoBlock("<project-directory>")} is required.`);
     // TODO: 补充更多内容
   })
   .action((projectDirectory: string) => {
@@ -40,7 +42,7 @@ program
   .parse(process.argv);
 
 if (!projectName) {
-  console.log(error('Please specify the project directory!'));
+  console.log(error("Please specify the project directory!"));
   process.exit(0);
 }
 
@@ -64,8 +66,8 @@ function checkProjectName(): void {
  * @param {[string]} results
  */
 function printValidationResults(results: [string]): void {
-  if (typeof results !== 'undefined') {
-    results.forEach((result) => {
+  if (typeof results !== "undefined") {
+    results.forEach(result => {
       console.error(error(`  *  ${result}`));
     });
   }
@@ -85,22 +87,22 @@ async function createProject() {
 
   await replacePkgConfig();
 
-  console.log('Installing packages. This might take a couple of minutes.');
-  console.log('Installing react, react-dom, and rocketact-script...');
+  console.log("Installing packages. This might take a couple of minutes.");
+  console.log("Installing react, react-dom, and rocketact-script...");
 
   try {
     await packageUtil.install();
-    console.log(`Installing packages. ${successBlock(' done ')}`);
+    console.log(`Installing packages. ${successBlock(" done ")}`);
   } catch (err) {
     console.log(err);
-    console.log(error('Install packages failed.'));
+    console.log(error("Install packages failed."));
   }
 
   console.log();
-  console.log(`${successBlock(' All Things done. ')}`);
+  console.log(`${successBlock(" All Things done. ")}`);
   console.log();
   console.log(success(`  cd ${projectName}`));
-  console.log(success('  yarn start'));
+  console.log(success("  yarn start"));
   console.log();
 }
 
@@ -112,11 +114,15 @@ async function createProject() {
  */
 async function copyTemplateFiles(projectDir: string) {
   try {
-    console.log('Initializing project content...');
-    await fs.copy(path.resolve(path.join(__dirname, '../../template')), projectDir, {
-      overwrite: true
-    });
-    console.log(`Initializing project content. ${successBlock(' done ')}`);
+    console.log("Initializing project content...");
+    await fs.copy(
+      path.resolve(path.join(__dirname, "../../template")),
+      projectDir,
+      {
+        overwrite: true
+      }
+    );
+    console.log(`Initializing project content. ${successBlock(" done ")}`);
   } catch (err) {
     console.error(err);
   }
@@ -124,9 +130,11 @@ async function copyTemplateFiles(projectDir: string) {
 
 /** reaplace pkg.json name */
 async function replacePkgConfig() {
-  const projectPkg = fs.readJsonSync(path.resolve(process.cwd(), 'package.json'));
+  const projectPkg = fs.readJsonSync(
+    path.resolve(process.cwd(), "package.json")
+  );
   projectPkg.name = projectName;
-  fs.writeJsonSync('./package.json', projectPkg, {
+  fs.writeJsonSync("./package.json", projectPkg, {
     spaces: 2,
     EOL: os.EOL
   });
