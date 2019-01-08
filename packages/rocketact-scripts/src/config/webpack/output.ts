f80d2aa60e3b97cc55f86fc96bfd2ae15ed16110
loveky <ylzcylx@gmail.com>
2019-01-08 17:24:53 +0800

    feat: add chunkhash to JS filename in production build

--- packages/rocketact-scripts/src/config/webpack/output.ts
+++ packages/rocketact-scripts/src/config/webpack/output.ts
@@ -11,10 +11,14 @@ import {
 
 export default (api: CoreAPI) => {
   api.chainWebpack(webpackChain => {
+    const filenameTemplate = isDevelopmentEnv()
+      ? "js/[name].bundle.js"
+      : "js/[name].[chunkhash:8].js";
+
     webpackChain.output
       .pathinfo(isDevelopmentEnv())
-      .filename("js/[name].bundle.js")
-      .chunkFilename("js/[name].bundle.js")
+      .filename(filenameTemplate)
+      .chunkFilename(filenameTemplate)
       .publicPath(
         isDevelopmentEnv()
           ? "/"
