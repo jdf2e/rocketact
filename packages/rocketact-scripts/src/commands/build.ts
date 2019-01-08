2e760b39d8a7ee1234c727a9474dde9915232c3c
loveky <ylzcylx@gmail.com>
2019-01-08 19:44:40 +0800

    feat: clean up build directory before build

--- packages/rocketact-scripts/src/commands/build.ts
+++ packages/rocketact-scripts/src/commands/build.ts
@@ -1,8 +1,16 @@
+const del = require("del");
 import webpack from "webpack";
+import path from "path";
 
 import CoreAPI from "../CoreAPI";
 
-import { error, errorBlock, success, successBlock } from "rocketact-dev-utils";
+import {
+  error,
+  errorBlock,
+  success,
+  successBlock,
+  appBuild
+} from "rocketact-dev-utils";
 
 export default (api: CoreAPI) => {
   api.registerCommand(
@@ -10,36 +18,40 @@ export default (api: CoreAPI) => {
     (): Promise<any> => {
       process.env.NODE_ENV = "production";
 
-      return new Promise((resolve, reject) => {
-        const startTime = +new Date();
-        webpack(api.resolveWebpackConfig(), (err, stats) => {
-          const endTime = +new Date();
-          const duration = ((endTime - startTime) / 100).toFixed(2);
+      return Promise.resolve()
+        .then(() => del(path.join(appBuild(), "**")))
+        .then(() => {
+          return new Promise((resolve, reject) => {
+            const startTime = +new Date();
+            webpack(api.resolveWebpackConfig(), (err, stats) => {
+              const endTime = +new Date();
+              const duration = ((endTime - startTime) / 100).toFixed(2);
 
-          if (err || stats.hasErrors()) {
-            console.log(
-              `${errorBlock(" ERROR ")} ${error(
-                `Build completed in ${duration}s`
-              )} with following error:`
-            );
-            if (err) {
-              console.log(error(`${err.name}: ${err.message}`));
-              reject(err);
-            } else {
-              const info = stats.toJson();
-              console.log(error(info.errors));
-              reject(info.errors);
-            }
-          } else {
-            console.log(
-              `${successBlock(" SUCCESS ")} ${success(
-                `Build completed in ${duration}s`
-              )}`
-            );
-            resolve();
-          }
+              if (err || stats.hasErrors()) {
+                console.log(
+                  `${errorBlock(" ERROR ")} ${error(
+                    `Build completed in ${duration}s`
+                  )} with following error:`
+                );
+                if (err) {
+                  console.log(error(`${err.name}: ${err.message}`));
+                  reject(err);
+                } else {
+                  const info = stats.toJson();
+                  console.log(error(info.errors));
+                  reject(info.errors);
+                }
+              } else {
+                console.log(
+                  `${successBlock(" SUCCESS ")} ${success(
+                    `Build completed in ${duration}s`
+                  )}`
+                );
+                resolve();
+              }
+            });
+          });
         });
-      });
     }
   );
 };
