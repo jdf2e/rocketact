29b09374fd0e9653f4e7772ea603acd41a4fb6f7
loveky <ylzcylx@gmail.com>
2019-02-14 10:46:04 +0800

    fix: allow - in plugin names

--- packages/rocketact-dev-utils/src/isPlugin.ts
+++ packages/rocketact-dev-utils/src/isPlugin.ts
@@ -1,5 +1,5 @@
 const isPlugin = (packageName: string): boolean => {
-  return !!packageName.match(/^rocketact-plugin-(\w)+$/);
+  return !!packageName.match(/^rocketact-plugin-([\w-])+$/);
 };
 
 export { isPlugin };
