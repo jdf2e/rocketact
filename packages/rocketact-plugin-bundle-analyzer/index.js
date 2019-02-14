42391cc23cfcfc842516dc39d11cfe25acfbac6f
loveky <ylzcylx@gmail.com>
2019-02-14 11:10:46 +0800

    fix

--- packages/rocketact-plugin-bundle-analyzer/index.js
+++ packages/rocketact-plugin-bundle-analyzer/index.js
@@ -1,14 +1,17 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const {isProductionEnv} = require("rocketact-scripts/dist/utils/environment");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const { isProductionEnv } = require("rocketact-scripts/dist/utils/environment");
 
 module.exports = api => {
-  if (isProductionEnv()) {
-    api.chainWebpack(webpackChain => {
-      webpackChain.plugin('WebpackBundleAnalyzer').use(BundleAnalyzerPlugin, [{
-        analyzerMode: 'static',
-        reportFilename: 'bundleAnalyzerReport.html',
-        openAnalyzer: false
-      }])
-    })
-  }
-}
+  api.chainWebpack(webpackChain => {
+    if (isProductionEnv()) {
+      webpackChain.plugin("WebpackBundleAnalyzer").use(BundleAnalyzerPlugin, [
+        {
+          analyzerMode: "static",
+          reportFilename: "bundleAnalyzerReport.html",
+          openAnalyzer: false
+        }
+      ]);
+    }
+  });
+};
