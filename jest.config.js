2815e3601d4ad9f6269c9df6286440f128fb1890
loveky <ylzcylx@gmail.com>
2019-02-20 21:54:02 +0800

    test: add repo levev jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsConfig: {
+        moduleResolution: "node",
+        esModuleInterop: true
+      }
+    }
+  }
+};
--- packages/rocketact-dev-utils/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverageFrom: ["src/**/*.{js,ts}"],
-  testMatch: [  "**/test/?(*.)+(spec|test).+(j|t)s?(x)" ]
-};
