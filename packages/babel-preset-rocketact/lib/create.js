7f6deaebc61d1f593220c0549e9557608d50ed5d
loveky <ylzcylx@gmail.com>
2019-05-08 20:39:33 +0800

    feat: enable loose mode for @babel/plugin-proposal-class-properties

--- packages/babel-preset-rocketact/lib/create.js
+++ packages/babel-preset-rocketact/lib/create.js
@@ -23,7 +23,7 @@ module.exports = function(api, options, env) {
           )
         }
       ],
-      require.resolve("@babel/plugin-proposal-class-properties"),
+      [require.resolve("@babel/plugin-proposal-class-properties"), {loose: true}],
       require.resolve("@babel/plugin-proposal-object-rest-spread"),
       require.resolve("@babel/plugin-syntax-dynamic-import"),
       require.resolve("babel-plugin-transform-react-remove-prop-types"),
